e1c439acfe629d6e072eb001c8e3c300129b20e5
Slothyspace <55360634+Slothyspace@users.noreply.github.com>
2022-10-02 19:34:07 -0400

    Update Card Deck Dealer.rb

--- Card Deck Dealer.rb
+++ Card Deck Dealer.rb
@@ -2,8 +2,8 @@
 module Card_Deck_Dealer
 
  class Card_deck
-   attr_accessor :card, :deck
-   attr_reader :card_library, :selection
+   attr_accessor :deck, :selection
+   attr_reader :card_library
     #initialize to generate card deck library (Working)
    def initialize
     @deck = []
@@ -43,61 +43,79 @@ module Card_Deck_Dealer
      end
     end
    end
-
-   #method to grab random card from hash without revealing it
-   #and then remove from temporary deck (WIP)
-   def draw
-     puts @card_library.count
-   end
-
-   #method to rebuild deck from card library (WIP)
-   def collect
+   
+   #reset temp deck (WIP) (Needs testing)
+   def reset_deck
      @deck = @card_library
    end
 
-   #method to pick a random hash from array,
-   #convert it to a readable sting and print it from card library (Working)
-   def show_one
-     @selection = @card_library.shuffle[0]
-     suit = @selection.keys
-     value = @selection.values
-     suit = suit.to_ary
-     value = value.to_ary
-     suit = suit[0].to_s
-     value = value[0].to_i
-     case suit
-     when "Joker"
-       if value == 2
-         value = "Big"
-       else
-         value = "Little"
+   #method to grab random card from array without revealing it
+   #and then remove selected card from temporary deck (WIP) (Needs testing)
+   def draw
+     card_count = @deck.length
+     random_num = rand(0..."#{card_count}")
+     card_choice = @deck.["#{random_num}"]
+     @selection = card_choice
+     @deck = @deck.delete_at("#{random_num}")
+     return @selection
+   end
+   
+   #method to convert selected card into readable format (WIP)(Needs testing)
+   def read_card
+     if @selection != nil
+       suit = @selection.keys
+       value = @selection.values
+       suit = suit.to_ary
+       value = value.to_ary
+       suit = suit[0].to_s
+       value = value[0].to_i
+       case suit
+         when "Joker"
+          if value == 2
+            value = "Big"
+          else
+            value = "Little"
+          end
        end
-     end
-     case value
-      when 11
-        value = "Jack"
-      when 12
-        value = "Queen"
-      when 13
-        value = "King"
-      when 1
-        value = "Ace"
-     end
-     if suit == "Joker"
-       puts "#{value} #{suit}"
-     else
-       puts "#{value} of #{suit}"
-     end
+       case value
+         when 11
+           value = "Jack"
+         when 12
+           value = "Queen"
+         when 13
+           value = "King"
+         when 1
+           value = "Ace"
+       end
+         if suit == "Joker"
+           puts "#{value} #{suit}"
+         else
+           puts "#{value} of #{suit}"
+         end
+      else
+        puts "No card selected."
+      end
+    end
    end
-   #method to show all cards in library in readable text (WIP)
-   def show_all
-     @card_library.each do |key, value|
-       puts "#{key} #{value}"
-     end
-   end
+ end
+end
+
+$deck = Card_deck.new
+
+  #pull card from deck, remove it and then output it into readable text (WIP) (Needs Testing)
+  def deal_one
+    $deck.draw
+    $deck.read_card
+  end
+
+ #method to pick a random hash from array and print it from card library (Needs testing)
+  def show_random
+     $deck.@selection = $deck.@card_library.shuffle[0]
+     $deck.read_card
   end
 end
 
 include Card_Deck_Dealer
 
-deck = Card_deck.new
+$deck.deal_one
+$deck.show_random
